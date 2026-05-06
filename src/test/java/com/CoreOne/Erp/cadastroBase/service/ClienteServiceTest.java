058f58adf60b0d105ca31727613779ada5f4a9a6
Otniel Marques <otnielmarques3@gmail.com>
2026-05-05 23:23:56 -0300

    Testes deletar e achar cliente por ID prontos

--- src/test/java/com/CoreOne/Erp/cadastroBase/service/ClienteServiceTest.java
+++ src/test/java/com/CoreOne/Erp/cadastroBase/service/ClienteServiceTest.java
@@ -11,6 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.mockito.Mockito.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -72,15 +80,46 @@ class ClienteServiceTest {
         assertEquals(clienteRequest.endereco(), clienteResponse.endereco());
         assertEquals(clienteRequest.email(), clienteResponse.email());
         assertEquals(clienteModel.getId(), clienteResponse.id());
-
     }
 
     @Test
     void listarCliente() {
+        List<ClienteModel> clienteModelList = new ArrayList<>(List.of(
+                new ClienteModel(1L, "Otniel Marques",
+                        TipoPessoa.FISICA,
+                        "123456789",
+                        "5547991986203", "Rua dos Bandeirantes, 79", "otnielmarques3@gmail.com"),
+                new ClienteModel(2L, "Philips", TipoPessoa.JURIDICA, "545487614196-00",
+                        "554712345678", "Rua dois de Setembro, 1234", "suporte@philips.com"),
+                new ClienteModel(3L, "Bella janela", TipoPessoa.JURIDICA, "12345678912",
+                        "554712345678", "Rua Goias, 1000", "suporte@bellajanela.com")
+        ));
+        Integer page = 0;
+        Integer size = 1;
+        when(clienteRepository.findAll(PageRequest.of(page, size))).thenReturn(new PageImpl<>(clienteModelList));
+        List<ClienteResponse> clienteResponseListCase1 = clienteService.listarCliente(null, null,
+                page, size);
+
+        assertEquals(clienteModelList.size(), clienteResponseListCase1.size());
     }
 
     @Test
     void listarClientePorId() {
+        ClienteModel clienteModel = new ClienteModel(1L, "Teste", TipoPessoa.JURIDICA, "111.222" +
+                ".333-4444", "123456789", "Rua do teste", "teste@email.com");
+
+        when(clienteRepository.findById(clienteModel.getId())).thenReturn(Optional.of(clienteModel));
+        Optional<ClienteResponse> clienteModelBanco = clienteService.listarClientePorId(clienteModel.getId());
+
+        verify(clienteRepository, times(1)).findById(clienteModel.getId());
+
+        assertEquals(clienteModel.getId(), clienteModelBanco.get().id());
+        assertEquals(clienteModel.getRazaoSocial(), clienteModelBanco.get().razaoSocial());
+        assertEquals(clienteModel.getTipoPessoa(), clienteModelBanco.get().tipoPessoa());
+        assertEquals(clienteModel.getDocumento(), clienteModelBanco.get().documento());
+        assertEquals(clienteModel.getTelefone(), clienteModelBanco.get().telefone());
+        assertEquals(clienteModel.getEndereco(), clienteModelBanco.get().endereco());
+        assertEquals(clienteModel.getEmail(), clienteModelBanco.get().email());
     }
 
     @Test
@@ -89,5 +128,13 @@ class ClienteServiceTest {
 
     @Test
     void deletarCliente() {
+        ClienteModel clienteModel = new ClienteModel(1L, "Teste", TipoPessoa.JURIDICA, "111.222" +
+                ".333-4444", "123456789", "Rua do teste", "teste@email.com");
+
+        when(clienteRepository.findById(clienteModel.getId())).thenReturn(Optional.of(clienteModel));
+
+        clienteService.deletarCliente(1L);
+
+        verify(clienteRepository, times(1)).delete(any(ClienteModel.class));
     }
 }
